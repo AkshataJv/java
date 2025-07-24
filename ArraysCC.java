ffb0204401762324a564788279d88582f8df3ec8
AkshataJv <akshata.mjv@gmail.com>
2025-07-24 12:34:12 +0530

    First Java File

---
+++ ArraysCC.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class ArraysCC {
+    public static void main(String[] args) {
+        int marks[]= new int[100];
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the length of array: "+marks.length);
+        // marks[0] = sc.nextInt();
+        // marks[1] = sc.nextInt();
+        // marks[2] = sc.nextInt();
+
+        // System.out.println("OP: "+marks[0]);
+        // System.out.println("DSA: "+marks[1]);
+        // System.out.println("Chem: "+marks[2]);
+    }
+}
