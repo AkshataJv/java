77c85c183958a2f32a39a3a91cb256e5015b78c1
AkshataJv <akshata.mjv@gmail.com>
2025-08-30 14:43:19 +0530

    Add

---
+++ Array.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Array {
+       public static void update(int marks[]){
+
+       }
+       public static void main(String[] args) {
+    //        int marks[] = new int[50];
+    //        Scanner sc = new Scanner(System.in);
+
+    //        System.out.println("Length of Array: "+marks.length);
+
+    //     marks[0] = sc.nextInt();
+    //     marks[1] = sc.nextInt();
+    //     marks[2] = sc.nextInt();
+    //     System.out.println("DS : " + marks[0]);
+    //     System.out.println("OS : " + marks[1]);
+    //     System.out.println("Maths : " + marks[2]);
+
+    //     int percentage = (marks[0]+ marks[1]+marks[2])/3;
+    //     System.out.println("Percentage : "+ percentage + "%");
+       }
+}
